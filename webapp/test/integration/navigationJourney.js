8d89ba325c71d65b8db0f7709c421e6390b60e71
Uladzimir Pryshchep <uladzimir.pryshchep@leverx.com>
2022-08-11 15:03:15 +0300

    [main] Start project

---
+++ webapp/test/integration/navigationJourney.js
@@ -0,0 +1,13 @@
+/*global QUnit, opaTest*/
+
+sap.ui.define([
+  "sap/ui/demo/walkthrough/localService/mockserver",
+  "sap/ui/test/opaQunit",
+  "./pages/App"
+], function (mockserver) {
+  "use strict";
+
+  QUnit.module("Navigation");
+
+
+});
